5ed4c0f2fcd1b407820c27f263c9649c8631dec2
Boisxon <boisxon357@gmail.com>
2024-12-14 17:21:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+
+
+
+const userName = prompt("Ism")
+const userAlpabet = prompt("Harif kiriting")
+
+if(userName.toLocaleLowerCase().includes(userAlpabet.toLocaleLowerCase())){
+    alert(`${userName} ismida ${userAlpabet} harfi mavjud`)
+}else{
+    alert(`${userName} ismida ${userAlpabet} harfi mavjud emas`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
